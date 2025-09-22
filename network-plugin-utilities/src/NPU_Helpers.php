c539db82057e7a44e7e235777028a4d9d9d2ae84
cdegourcy <cyrille@gourcy.net>
2025-09-22 13:48:44 +0200

    dev

--- network-plugin-utilities/src/NPU_Helpers.php
+++ network-plugin-utilities/src/NPU_Helpers.php
@@ -9,14 +9,95 @@ class NPU_Helpers {
         add_filter( 'register_post_type_args', [ __CLASS__, 'track_cpt_origin' ], 10, 2 );
         add_filter( 'register_taxonomy_args', [ __CLASS__, 'track_taxo_origin' ], 10, 2 );
 
-        // Menu réseau
+        // Menu réseau (admin network)
         add_action('network_admin_menu', [__CLASS__, 'register_menu']);
 
-        // Screen options
+        // Screen options (colonnes, pagination)
         add_filter('set-screen-option', [__CLASS__, 'set_screen_option'], 10, 3);
 
         // CSS admin
         add_action('admin_enqueue_scripts', [__CLASS__, 'enqueue_assets']);
+
+        // Shortcode pour affichage dynamique d'un menu des sites
+        add_shortcode('network_sites_menu', [__CLASS__, 'shortcode_network_sites_menu']);
+    }
+
+    /**
+     * Vérifie si le menu des sites du réseau est activé
+     */
+    protected static function is_menu_enabled() {
+        // Option stockée en base réseau, activée par défaut
+        return (bool) get_site_option('npu_enable_network_menu', true);
+    }
+
+    /**
+     * Récupère les sites du réseau
+     */
+    protected static function get_sites_list() {
+        // Si désactivé via l’option → ne rien renvoyer
+        if ( ! self::is_menu_enabled() ) {
+            return [];
+        }
+
+        $sites = get_sites([
+            'public'   => 1,
+            'archived' => 0,
+            'deleted'  => 0
+        ]);
+
+        $out = [];
+        foreach ( $sites as $site ) {
+            $out[] = [
+                'id'   => $site->blog_id,
+                'url'  => get_site_url( $site->blog_id ),
+                'name' => get_blog_option( $site->blog_id, 'blogname' )
+            ];
+        }
+        return $out;
+    }
+
+    /**
+     * Affiche ou retourne la liste HTML des sites
+     */
+    public static function render_sites_list( $wrapper = 'ul', $class = 'network-sites-menu', $echo = true ) {
+        $sites = self::get_sites_list();
+        if ( empty( $sites ) ) {
+            return '';
+        }
+
+        ob_start();
+        echo '<' . tag_escape( $wrapper ) . ' class="' . esc_attr( $class ) . '">';
+        foreach ( $sites as $site ) {
+            echo '<li><a href="' . esc_url( $site['url'] ) . '">' . esc_html( $site['name'] ) . '</a></li>';
+        }
+        echo '</' . tag_escape( $wrapper ) . '>';
+
+        $html = ob_get_clean();
+
+        if ( $echo ) {
+            echo $html;
+        }
+        return $html;
+    }
+
+    /**
+     * Fonction d’appel direct en PHP
+     * Usage : <?php NPU_Helpers::network_sites_menu(); ?>
+     */
+    public static function network_sites_menu( $wrapper = 'ul', $class = 'network-sites-menu' ) {
+        return self::render_sites_list( $wrapper, $class, true );
+    }
+
+    /**
+     * Shortcode [network_sites_menu]
+     */
+    public static function shortcode_network_sites_menu( $atts ) {
+        $atts = shortcode_atts([
+            'class'   => 'network-sites-menu',
+            'wrapper' => 'ul'
+        ], $atts );
+
+        return self::render_sites_list( $atts['wrapper'], $atts['class'], false );
     }
 
     /**
@@ -84,9 +165,10 @@ class NPU_Helpers {
     }
 
     /**
-     * Menu réseau
+     * Menu réseau (pages dans l’admin réseau)
      */
     public static function register_menu() {
+        // Page principale d’analyse
         $hook = add_submenu_page(
             'sites.php',
             __('Analyse du réseau (MU)', 'rdc-core-mu-utilities'),
@@ -96,6 +178,7 @@ class NPU_Helpers {
             [__CLASS__, 'render_page']
         );
 
+        // Options d’écran
         add_action("load-$hook", function() {
             add_screen_option('per_page', [
                 'label'   => __('Sites par page', 'rdc-core-mu-utilities'),
@@ -108,6 +191,49 @@ class NPU_Helpers {
                 'default' => 5,
             ]);
         });
+
+        // Page d’options NPU
+        add_submenu_page(
+            'settings.php',
+            __('Options NPU', 'rdc-core-mu-utilities'),
+            __('Options NPU', 'rdc-core-mu-utilities'),
+            'manage_network_options',
+            'npu-options',
+            [__CLASS__, 'render_options_page']
+        );
+    }
+
+    /**
+     * Page d’options du plugin (admin réseau)
+     */
+    public static function render_options_page() {
+        // Sauvegarde si POST
+        if ( isset($_POST['npu_save']) && check_admin_referer('npu_save_options') ) {
+            update_site_option('npu_enable_network_menu', ! empty($_POST['npu_enable_network_menu']));
+            echo '<div class="updated"><p>' . __('Options sauvegardées', 'rdc-core-mu-utilities') . '</p></div>';
+        }
+
+        $enabled = self::is_menu_enabled();
+        ?>
+        <div class="wrap">
+            <h1><?php _e('Options NPU', 'rdc-core-mu-utilities'); ?></h1>
+            <form method="post">
+                <?php wp_nonce_field('npu_save_options'); ?>
+                <table class="form-table">
+                    <tr>
+                        <th scope="row"><?php _e('Activer le menu réseau', 'rdc-core-mu-utilities'); ?></th>
+                        <td>
+                            <label>
+                                <input type="checkbox" name="npu_enable_network_menu" value="1" <?php checked($enabled); ?>>
+                                <?php _e('Oui, afficher la liste des sites du réseau', 'rdc-core-mu-utilities'); ?>
+                            </label>
+                        </td>
+                    </tr>
+                </table>
+                <?php submit_button(__('Enregistrer', 'rdc-core-mu-utilities'), 'primary', 'npu_save'); ?>
+            </form>
+        </div>
+        <?php
     }
 
     /**
@@ -132,16 +258,22 @@ class NPU_Helpers {
     }
 
     /**
-     * Rendu de la page
+     * Rendu de la page principale
      */
     public static function render_page() {
         echo '<div class="wrap">';
         echo '<h1>' . esc_html__('Vue d’ensemble des sites du réseau', 'rdc-core-mu-utilities') . '</h1>';
 
-        $table = new NPU_List_Table();
-        $table->prepare_items();
-        $table->display();
+        // ⚠️ S’assurer que la classe NPU_List_Table existe
+        if ( class_exists('NPU_List_Table') ) {
+            $table = new NPU_List_Table();
+            $table->prepare_items();
+            $table->display();
+        } else {
+            echo '<p>' . __('Erreur : classe NPU_List_Table manquante.', 'rdc-core-mu-utilities') . '</p>';
+        }
 
         echo '</div>';
     }
+
 }
---
+++ network-plugin-utilities/src/NPU_Network_Sites_Menu.php
@@ -0,0 +1,122 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class NPU_Network_Sites_Menu {
+
+    public static function init() {
+        // Admin : ajouter une meta box dans les menus
+        add_action( 'admin_init', [ __CLASS__, 'add_nav_menu_metabox' ] );
+
+        // Shortcode : [network_sites_menu]
+        add_shortcode( 'network_sites_menu', [ __CLASS__, 'shortcode_network_sites_menu' ] );
+    }
+
+    /**
+     * Récupère les sites du réseau
+     */
+    protected static function get_sites_list() {
+        $sites = get_sites([
+            'public'   => 1,
+            'archived' => 0,
+            'deleted'  => 0
+        ]);
+
+        $out = [];
+        foreach ( $sites as $site ) {
+            $out[] = [
+                'id'   => $site->blog_id,
+                'url'  => get_site_url( $site->blog_id ),
+                'name' => get_blog_option( $site->blog_id, 'blogname' )
+            ];
+        }
+        return $out;
+    }
+
+    /**
+     * Ajout d’une metabox "Sites du réseau" dans l’écran Apparence > Menus
+     */
+    public static function add_nav_menu_metabox() {
+        add_meta_box(
+            'network_sites_nav_links',
+            __( 'Sites du réseau', 'rdc-core' ),
+            [ __CLASS__, 'render_nav_menu_metabox' ],
+            'nav-menus',
+            'side',
+            'default'
+        );
+    }
+
+    public static function render_nav_menu_metabox() {
+        $sites = self::get_sites_list();
+        ?>
+        <div id="posttype-network-sites" class="posttypediv">
+            <div id="tabs-panel-network-sites" class="tabs-panel tabs-panel-active">
+                <ul id="network-sites-checklist" class="categorychecklist form-no-clear">
+                    <?php foreach ( $sites as $site ) : ?>
+                        <li>
+                            <label class="menu-item-title">
+                                <input type="checkbox"
+                                    class="menu-item-checkbox"
+                                    name="menu-item[<?php echo esc_attr( $site['id'] ); ?>][menu-item-object-id]"
+                                    value="<?php echo esc_attr( $site['id'] ); ?>" />
+                                <?php echo esc_html( $site['name'] ); ?>
+                            </label>
+                            <input type="hidden" class="menu-item-type" value="custom" name="menu-item[<?php echo esc_attr( $site['id'] ); ?>][menu-item-type]" />
+                            <input type="hidden" class="menu-item-title" value="<?php echo esc_attr( $site['name'] ); ?>" name="menu-item[<?php echo esc_attr( $site['id'] ); ?>][menu-item-title]" />
+                            <input type="hidden" class="menu-item-url" value="<?php echo esc_url( $site['url'] ); ?>" name="menu-item[<?php echo esc_attr( $site['id'] ); ?>][menu-item-url]" />
+                            <input type="hidden" class="menu-item-status" value="publish" name="menu-item[<?php echo esc_attr( $site['id'] ); ?>][menu-item-status]" />
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+            <p class="button-controls">
+                <span class="add-to-menu">
+                    <input type="submit"<?php disabled( empty( $sites ) ); ?> class="button-secondary submit-add-to-menu right" value="<?php esc_attr_e( 'Ajouter au menu' ); ?>" name="add-post-type-menu-item" id="submit-network-sites" />
+                    <span class="spinner"></span>
+                </span>
+            </p>
+        </div>
+        <?php
+    }
+
+    /**
+     * Affichage dynamique (shortcode)
+     */
+    public static function shortcode_network_sites_menu( $atts ) {
+        $atts = shortcode_atts([
+            'class'   => 'network-sites-menu',
+            'wrapper' => 'ul'
+        ], $atts );
+
+        return self::render_sites_list( $atts['wrapper'], $atts['class'] );
+    }
+
+    /**
+     * Rend la liste HTML des sites
+     */
+    public static function render_sites_list( $wrapper = 'ul', $class = 'network-sites-menu' ) {
+        $sites = self::get_sites_list();
+        if ( empty( $sites ) ) {
+            return '';
+        }
+
+        ob_start();
+        echo '<' . tag_escape( $wrapper ) . ' class="' . esc_attr( $class ) . '">';
+        foreach ( $sites as $site ) {
+            echo '<li><a href="' . esc_url( $site['url'] ) . '">' . esc_html( $site['name'] ) . '</a></li>';
+        }
+        echo '</' . tag_escape( $wrapper ) . '>';
+        return ob_get_clean();
+    }
+}
+
+// Boot
+NPU_Network_Sites_Menu::init();
+
+/**
+ * Fonction helper globale pour l’appel direct en PHP
+ */
+function rdc_network_sites_menu( $wrapper = 'ul', $class = 'network-sites-menu' ) {
+    echo RDC_Network_Sites_Menu::render_sites_list( $wrapper, $class );
+}
