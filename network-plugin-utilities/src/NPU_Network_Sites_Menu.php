f0298497f4ac8bda8fbbda2c5d852e3cfc21d893
cdegourcy <cyrille@gourcy.net>
2025-09-26 12:19:11 +0200

    dev

--- network-plugin-utilities/src/NPU_Network_Sites_Menu.php
+++ network-plugin-utilities/src/NPU_Network_Sites_Menu.php
@@ -66,19 +66,48 @@ class NPU_Network_Sites_Menu {
         <div id="posttype-network-sites" class="posttypediv">
             <div id="tabs-panel-network-sites" class="tabs-panel tabs-panel-active">
                 <ul id="network-sites-checklist" class="categorychecklist form-no-clear">
-                    <?php foreach ( $sites as $site ) : ?>
+                    <?php foreach ( $sites as $i => $site ) : 
+                        $item_id = - ( $i + 1 );
+                    ?>
                         <li>
                             <label class="menu-item-title">
                                 <input type="checkbox"
                                     class="menu-item-checkbox"
-                                    name="menu-item[<?php echo esc_attr( $site['id'] ); ?>][menu-item-object-id]"
-                                    value="<?php echo esc_attr( $site['id'] ); ?>" />
+                                    name="menu-item[<?php echo esc_attr( $item_id ); ?>][menu-item-object-id]"
+                                    value="-1" />
                                 <?php echo esc_html( $site['name'] ); ?>
                             </label>
-                            <input type="hidden" class="menu-item-type" value="custom" name="menu-item[<?php echo esc_attr( $site['id'] ); ?>][menu-item-type]" />
-                            <input type="hidden" class="menu-item-title" value="<?php echo esc_attr( $site['name'] ); ?>" name="menu-item[<?php echo esc_attr( $site['id'] ); ?>][menu-item-title]" />
-                            <input type="hidden" class="menu-item-url" value="<?php echo esc_url( $site['url'] ); ?>" name="menu-item[<?php echo esc_attr( $site['id'] ); ?>][menu-item-url]" />
-                            <input type="hidden" class="menu-item-status" value="publish" name="menu-item[<?php echo esc_attr( $site['id'] ); ?>][menu-item-status]" />
+
+                            <input type="hidden" class="menu-item-db-id"
+                                name="menu-item[<?php echo esc_attr( $item_id ); ?>][menu-item-db-id]"
+                                value="0" />
+                            <input type="hidden" class="menu-item-object"
+                                name="menu-item[<?php echo esc_attr( $item_id ); ?>][menu-item-object]"
+                                value="custom" />
+                            <input type="hidden" class="menu-item-parent-id"
+                                name="menu-item[<?php echo esc_attr( $item_id ); ?>][menu-item-parent-id]"
+                                value="0" />
+                            <input type="hidden" class="menu-item-type"
+                                name="menu-item[<?php echo esc_attr( $item_id ); ?>][menu-item-type]"
+                                value="custom" />
+                            <input type="hidden" class="menu-item-title"
+                                name="menu-item[<?php echo esc_attr( $item_id ); ?>][menu-item-title]"
+                                value="<?php echo esc_attr( $site['name'] ); ?>" />
+                            <input type="hidden" class="menu-item-url"
+                                name="menu-item[<?php echo esc_attr( $item_id ); ?>][menu-item-url]"
+                                value="<?php echo esc_url( $site['url'] ); ?>" />
+                            <input type="hidden" class="menu-item-target"
+                                name="menu-item[<?php echo esc_attr( $item_id ); ?>][menu-item-target]"
+                                value="" />
+                            <input type="hidden" class="menu-item-attr-title"
+                                name="menu-item[<?php echo esc_attr( $item_id ); ?>][menu-item-attr-title]"
+                                value="" />
+                            <input type="hidden" class="menu-item-classes"
+                                name="menu-item[<?php echo esc_attr( $item_id ); ?>][menu-item-classes]"
+                                value="" />
+                            <input type="hidden" class="menu-item-xfn"
+                                name="menu-item[<?php echo esc_attr( $item_id ); ?>][menu-item-xfn]"
+                                value="" />
                         </li>
                     <?php endforeach; ?>
                 </ul>
@@ -86,10 +115,10 @@ class NPU_Network_Sites_Menu {
             <p class="button-controls">
                 <span class="add-to-menu">
                     <input type="submit"<?php disabled( empty( $sites ) ); ?>
-                           class="button-secondary submit-add-to-menu right"
-                           value="<?php esc_attr_e( 'Ajouter au menu' ); ?>"
-                           name="add-post-type-menu-item"
-                           id="submit-network-sites" />
+                        class="button-secondary submit-add-to-menu right"
+                        value="<?php esc_attr_e( 'Ajouter au menu' ); ?>"
+                        name="add-custom-menu-item"
+                        id="submit-network-sites" />
                     <span class="spinner"></span>
                 </span>
             </p>
